73d4c194d7f19a87f0a66b0c61e32da1cb02ccae
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-03-12 09:07:08 +0500

    fixed banner ui

--- src/app/components/banner/banner.tsx
+++ src/app/components/banner/banner.tsx
@@ -13,7 +13,22 @@ import Header from "../header/header";
 const Banner = () => {
   gsap.registerPlugin(ScrollTrigger);
 
-  const b2Images = [Erkenbar, Effizient, Einheimisch];
+  const b2Images = [
+    {
+      imgSrc: Erkenbar,
+      styles: "max-w-[55rem] w-full",
+    },
+
+    {
+      imgSrc: Effizient,
+      styles: "max-w-[44rem] w-full",
+    },
+
+    {
+      imgSrc: Einheimisch,
+      styles: "max-w-[58rem] w-full",
+    },
+  ];
 
   const overLappingPanel = useRef<HTMLElement | null>(null);
   const sliderContainer = useRef<HTMLDivElement | null>(null);
@@ -64,7 +79,9 @@ const Banner = () => {
             ease: "power4.inOut",
             clearProps: "all",
             className: "slide h-full w-full absolute opacity-1 flex opacity-0",
-            duration: 1,
+            // className:
+            //   "slide max-w-[650px] w-full h-[210px] absolute opacity-1 flex opacity-0",
+            // duration: 1,
           })
           .to(
             slideTo,
@@ -73,6 +90,8 @@ const Banner = () => {
               autoAlpha: 1,
               ease: "power4.inOut",
               className: "active slide h-full w-full absolute opacity-0 flex",
+              // className:
+              //   "active slide max-w-[650px] w-full h-[210px] absolute opacity-0 flex",
               duration: 1,
             },
             0,
@@ -137,17 +156,17 @@ const Banner = () => {
         ref={overLappingPanel}
         className="panel h-screen bg-smoke-red relative flex items-center justify-center overflow-hidden"
       >
-        <div className="flex flex-col max-w-[46rem] px-[1rem] sm:px-[2rem] md:px-0">
+        <div className="flex flex-col px-[1rem] sm:px-[2rem] md:px-0">
           <Image
             src={BannerLogo}
             width={200}
             height={200}
             alt="banner-logo"
-            className="w-full"
+            className="w-full max-w-[45rem]"
             priority
           />
-          <h2 className="text-white text-lg sm:text-4xl uppercase text-center mt-8 md:mt-12">
-            ZUHAUSE IM ROTEN FELD - LÜNEBURG
+          <h2 className="text-white text-lg sm:text-4xl uppercase text-center font-gibson-medium mt-8 tracking-widest sm:mt-14">
+            ZUHAUSE IM ROTEN FELD <span className="font-area">-</span> LÜNEBURG
           </h2>
         </div>
       </section>
@@ -157,21 +176,23 @@ const Banner = () => {
           ref={sliderContainer}
           className="h-screen bg-light-purple relative top-0 left-0 w-full flex items-center justify-center px-[1rem] sm:px-[2rem] z-20"
         >
-          <div className="max-w-[650px] w-full h-[210px] md:px-0 relative">
+          <div className="w-full h-[210px] flex items-center justify-center md:px-0 relative">
             {b2Images.map((image, index) => (
-              <div
-                key={index}
-                className={`slide h-full w-full absolute flex ${
-                  index === 0 && "active"
-                } ${index !== 0 && "opacity-0"}`}
-              >
-                <Image
-                  src={image}
-                  fill
-                  alt={`banner-slider-image-${index}`}
-                  className="w-full"
-                  priority
-                />
+              <div className={`${image.styles} h-full absolute`}>
+                <div
+                  key={index}
+                  className={`slide w-full h-full flex ${
+                    index === 0 && "active"
+                  } ${index !== 0 && "opacity-0"}`}
+                >
+                  <Image
+                    src={image.imgSrc}
+                    fill
+                    alt={`banner-slider-image-${index}`}
+                    className={`w-full ${image.styles}`}
+                    priority
+                  />
+                </div>
               </div>
             ))}
           </div>
