e4f13f67f6c36a71ff1a2e13d2c4a05aafbe6429
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-03-13 11:30:09 +0500

    fixed contact form bugs

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -2,14 +2,14 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import sendgrid from "@sendgrid/mail";
 
-sendgrid.setApiKey(process.env.EMAIL_API_KEY!);
+sendgrid.setApiKey(process.env.NEXT_PUBLIC_EMAIL_API_KEY!);
 
 export async function POST(request: NextRequest) {
   const data = await request.json();
   try {
     const msg: any = {
-      to: process.env.SENDGRID_EMAIL_TO, // Your email where you'll receive emails
-      from: process.env.SENDGRID_EMAIL_FROM, // your website email address here
+      to: process.env.NEXT_PUBLIC_SENDGRID_EMAIL_TO, // Your email where you'll receive emails
+      from: process.env.NEXT_PUBLIC_SENDGRID_EMAIL_FROM, // your website email address here
       replyTo: data?.email,
       body: data.message,
       subject: `[Lead from website]`,
