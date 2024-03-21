a51810bb5457f484033e881ed903431c9ad992e8
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-03-21 11:21:09 +0500

    updated content on das-objekt section

--- src/app/components/das-objekt/das-objekt.tsx
+++ src/app/components/das-objekt/das-objekt.tsx
@@ -80,29 +80,38 @@ const DasObjekt = () => {
 
         {/*  */}
 
-        <p className="lg:w-[70vw] xl:w-[56.67vw] lg:flex gap-0 ml-auto lg:gap-7 xl:gap-4 pt-5 pb-10 md:pt-[4.75vw] md:pb-[13.8vw] lg:mr-[140px]">
-          <span className="inline-block mb-5 lg:mb-0 flex-1 text-[clamp(1rem,1.2vw,1.375rem)] text-black 2xl:leading-[2.159rem]">
-            Das Gebäudeensemble besteht aus einem linearen Baukörper, der sich
-            entlang der Willy-Brandt-Straße positioniert sowie einem
-            freiste-henden Punkthaus auf der lärmabge-wandten Seite am
-            Rotenbleicher Weg. Der Riegel bildet im Norden ein Hochpunkt aus und
-            stuft sich zum Rotenbleicher Weg in Richtung Süden ab. Durch die
-            Aufnahmen von Winkeln, Fluchten und Paral-lelen bilden beide Gebäude
-            ein Ensemble und formieren – abgeschirmt von der Willy-Brandt-Straße
-            – einen Freiraum, der den Anwohnern als Aufenthaltsfläche dient und
-            ausreichend Spiel-raum für durchgrünte Außenräume und Kinder-
+        <p className="lg:w-[70vw] xl:w-[56.67vw] lg:flex gap-0 ml-auto lg:gap-7 xl:gap-4 pt-5 pb-10 md:pt-[5.3vw] md:pb-[13.8vw] lg:mr-[140px]">
+          <span className="mb-5 lg:mb-0 flex-1 text-[clamp(1rem,1.2vw,1.375rem)] text-black 2xl:leading-[2.159rem]">
+            <span className="">
+              Immobilien gibt es viele, aber eine Immobilie unmittelbarer Nähe
+              der Altstadt, des Kurparks und des idyllischen Flüsschens Ilmenau
+              ist selten. Hier fügt sich der Neubau mit seinen zwei Gebäuden
+              ebenso sensibel wie feinsinnig in die zahlreichen
+              Gründerzeitbauten des Quartiers ein und versteht es, mit ihnen und
+              ihrer glanzvollen Architektur zu harmonieren.
+            </span>
+
+            <span className="mt-9 lg:inline-block">
+              Es ist eine schöne Symbiose aus städtischem Wohnen und Natur, die
+              im Rotenbleicher Weg stattfindet. Denn durch die Aufnahmen von
+              Winkeln, Fluchten und Parallelen formieren die
+            </span>
           </span>
 
-          <span className="inline-block flex-1 text-[clamp(1rem,1.2vw,1.375rem)] text-black 2xl:leading-[2.159rem]">
-            spielflächen bietet. Es entstehen insgesamt 71 Wohnungen mit 1 bis 5
-            Zimmern; 30% sind miet-preisgebunden. Mauris id ante lectus. Nunc
-            non lectus vel massa auctor dapibus. Sed convallis viverra lorem.
-            Nam nec nisl tellus. Fusce conse-quat nisi vitae nunc pellentesque,
-            ut congue tur-pis euis-mod. Sed tincidunt consequat dui non ut sit
-            amet nisi sit amet dolor dictum vestibulum. Cras egestas commodo
-            nunc, eu dignissim tellus molestie sed. In non ante quis mi viverra
-            sceler-isque. Suspendisse vel leo viverra, vestibulum dolor
-            volutpat, suscipit metus. Fusce consequat nisi vitae nunc pellente.
+          <span className="flex-1 text-[clamp(1rem,1.2vw,1.375rem)] text-black 2xl:leading-[2.159rem]">
+            <span className="">
+              beiden Gebäude ein zentrales xx qm2 großes, grünes Refugium mit
+              hoher Erholungsqualität, schattenspendenden Bäumen, Grünflächen,
+              Sonnen- und Spielplätzen.
+            </span>
+            <span className="mt-4 lg:mt-9 inline-block">
+              Eingerahmt von 71 1- bis 5-Zimmer-Wohnungen für Singles, Paare und
+              Familien. Mit sonnigen Terrassen oder Balkonen für schöne Blicke
+              ins Grüne oder über die ganze Stadt. Unter der Erde findet sich
+              eine Tiefgarage mit 57 PKW-Stellplätzen und 160 für Fahrräder,
+              inklusive Ladestationen. Um Lüneburg und Umgebung – mal so, mal so
+              – zu erkunden.
+            </span>
           </span>
         </p>
       </div>
