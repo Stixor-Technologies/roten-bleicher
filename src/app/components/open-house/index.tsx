a5030a867d5eb806b553206661fe8cf29023c079
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-09-18 16:21:01 +0500

    handle font size

--- src/app/components/open-house/index.tsx
+++ src/app/components/open-house/index.tsx
@@ -41,8 +41,8 @@ const OpenHouse = () => {
       styles="max-w-[550px]"
     >
       <div className="bg-light-purple p-3">
-        <div className="flex flex-col items-center text-center justify-center border-2 border-white p-4 py-20 gap-6 text-red ">
-          <h2 className="text-5xl font-area-extrabold uppercase mt-4">
+        <div className="flex flex-col items-center text-center justify-center border-2 border-white p-4 py-20 gap-3 sm:gap-6 text-red ">
+          <h2 className="text-3xl sm:text-5xl font-area-extrabold uppercase mt-4">
             Open House
           </h2>
           <span className="font-area-semibold">
